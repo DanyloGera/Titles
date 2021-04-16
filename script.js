f23421aa4b60ebe6f5adb34078f59df4d77a1ca6
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-04-16 19:29:54 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var position = 0;
+
+function scroller() {
+  if (position != 2400)
+     {position++;
+      scroll(0,position);
+      clearTimeout(timer);
+      var timer = setTimeout("scroller()",5); timer;
+     }
+  else
+     document.body.style.overflow = "visible";
+
+}
+
+
+
+// // setup game when the web page loads
+// window.onload = function () {
+//   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+//   // before the game begins
+//     function preload() {
+//       game.load.spritesheet('player', 'chalkers.png', 48, 62);
+//     }
+//     function create() {
+//       player = game.add.sprite(60, 480, 'player');
+//         player.animations.add('walk');
+//         player.anchor.setTo(0.5, 1);
+//     }
+//     function update() {
+// player.animations.play('walk', true)
+//     }
+//     function render() {
+//
+// }
+//   }
