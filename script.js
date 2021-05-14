4b7b8119e3d5bc5a7867891213f87dea46841099
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-14 17:54:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,23 +19,3 @@ function disabled1() {
 var btn = document.getElementById('btn');
 btn.setAttribute("disabled", "disabled");
 }
-
-// // setup game when the web page loads
-// window.onload = function () {
-//   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-//   // before the game begins
-//     function preload() {
-//       game.load.spritesheet('player', 'chalkers.png', 48, 62);
-//     }
-//     function create() {
-//       player = game.add.sprite(60, 480, 'player');
-//         player.animations.add('walk');
-//         player.anchor.setTo(0.5, 1);
-//     }
-//     function update() {
-// player.animations.play('walk', true)
-//     }
-//     function render() {
-//
-// }
-//   }
