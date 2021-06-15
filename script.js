0dd5d159808158814e5a3e44cbb2fcaf9aa4e524
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-06-15 19:35:25 +0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var position = 0;
-
-function scroller() {
-  if (position != 2700)
-     {position++;
-      scroll(0,position);
-      clearTimeout(timer);
-      var timer = setTimeout("scroller()",5); timer;
-     }
-  else
-     document.body.style.overflow = "visible";
-
-}
-
-window.onbeforeunload = function () {
-    window.scrollTo(0,0);
-};
-function disabled1() {
-var btn = document.getElementById('btn');
-btn.setAttribute("disabled", "disabled");
-}
+// var position = 0;
+//
+// function scroller() {
+//   if (position != 2700)
+//      {position++;
+//       scroll(0,position);
+//       clearTimeout(timer);
+//       var timer = setTimeout("scroller()",5); timer;
+//      }
+//   else
+//      document.body.style.overflow = "visible";
+//
+// }
+//
+// window.onbeforeunload = function () {
+//     window.scrollTo(0,0);
+// };
+// function disabled1() {
+// var btn = document.getElementById('btn');
+// btn.setAttribute("disabled", "disabled");
+// }
